b5645bd5f8609093c4d92b8c2551d912a2957953
Pratham Pathak <92325181+PrathamPathak0502@users.noreply.github.com>
2025-01-06 15:06:10 +0530

    Update BulletController.cs

--- BulletController.cs
+++ BulletController.cs
@@ -1,3 +1,7 @@
+//make a new scene in unity
+//for making bullet from game object select sphere 
+//now create a c# script for that bullet as this
+//attach this script to sphere(rename it as bullet for better understanding during developing game) in hierarchy
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,7 +16,9 @@ public class BulletController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   if(transform.position.z>= 15.0f) //this will help to give the bullet a distance how much it should travel 
+            Destroy(this.gameObject);
+    else
         transform.Translate(Vector3.forward*5.0f* Time.deltaTime);
     }
 }
